d397814d9c2f23659d1e90e085929a0dfe0f7f10
Roy van de Water <dev+roy@integrumtech.com>
2013-10-28 08:21:16 -0700

    Create model files before collection files

--- lib/generators/backbone/collection/collection_generator.rb
+++ lib/generators/backbone/collection/collection_generator.rb
@@ -2,20 +2,20 @@ module Backbone
   class CollectionGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
-    def copy_collection_file
-      template 'collection.js.coffee', "app/assets/javascripts/collections/#{plural_file_name}.js.coffee"
-    end
-
     def copy_model_file
       template 'model.js.coffee', "app/assets/javascripts/models/#{singular_file_name}.js.coffee"
     end
 
-    def copy_spec_file
-      template 'collection_spec.js.coffee', "spec/javascripts/collections/#{plural_file_name}_spec.js.coffee"
+    def copy_collection_file
+      template 'collection.js.coffee', "app/assets/javascripts/collections/#{plural_file_name}.js.coffee"
     end
 
     def copy_model_spec_file
       template 'model_spec.js.coffee', "spec/javascripts/models/#{singular_file_name}_spec.js.coffee"
+    end
+
+    def copy_spec_file
+      template 'collection_spec.js.coffee', "spec/javascripts/collections/#{plural_file_name}_spec.js.coffee"
     end
 
     protected
