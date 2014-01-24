ad4fb30871d0252cf0e8066f8b1cecee5f6d49cd
Jade Meskill <jade@integrumtech.com>
2014-01-23 21:03:46 -0700

    fix model generator

--- lib/generators/backbone/model/model_generator.rb
+++ lib/generators/backbone/model/model_generator.rb
@@ -3,11 +3,24 @@ module Backbone
     source_root File.expand_path('../templates', __FILE__)
 
     def copy_model_file
-      template 'model.js.coffee', "app/assets/javascripts/models/#{file_name}.js.coffee"
+      template 'model.js.coffee', "app/assets/javascripts/models/#{singular_file_name}.js.coffee"
     end
 
     def copy_spec_file
-      template 'model_spec.js.coffee', "spec/javascripts/models/#{file_name}_spec.js.coffee"
+      template 'model_spec.js.coffee', "spec/javascripts/models/#{singular_file_name}_spec.js.coffee"
+    end
+
+    protected
+    def plural_class_name
+      @plural_class_name ||= class_name.pluralize
+    end
+
+    def singular_class_name
+      @singular_class_name ||= class_name.singularize
+    end
+
+    def singular_file_name
+      @singular_file_name ||= file_name.singularize
     end
   end
 end
